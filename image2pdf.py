8e199ff2c0f88d167e04d92a4ea348ca7866db02
Jim Lin <b04705003@ntu.edu.tw>
2020-04-12 17:43:03 +0800

    add png support, catch exif exception, and add convert to RGB channel

--- image2pdf.py
+++ image2pdf.py
@@ -12,7 +12,10 @@ def exif_rotate(image):
 
     if not has_exif: return image
 
-    exif=dict(image._getexif().items())
+    try:
+        exif=dict(image._getexif().items())
+    except AttributeError:
+        return image
     if exif[orientation] == 3:
         image=image.rotate(180, expand=True)
     elif exif[orientation] == 6:
@@ -32,17 +35,18 @@ current = Path(args.path)
 paths_and_indices = []
 
 for image_p in current.iterdir():
-    if image_p.is_file() and re.search("\.jpe?g$", image_p.name) is not None:
-        number = re.search("[0-9]+", image_p.name)
+    if image_p.is_file() and re.search("\.(jpe?g|png)$", image_p.name) is not None:
+        number_only_name = re.sub("[^0-9]", "", image_p.name)
+        number = re.search("[0-9]+", number_only_name)
         paths_and_indices.append((image_p, 0 if number is None else int(number.group(0))))
 
 paths_and_indices.sort(key=lambda e: e[1])
 
 start_image_p = paths_and_indices[0][0]
-start_image = exif_rotate(Image.open(start_image_p))
+start_image = exif_rotate(Image.open(start_image_p).convert("RGB"))
 append_images = []
 for image_p, _ in paths_and_indices[1:]:
-    image = Image.open(image_p)
+    image = Image.open(image_p).convert("RGB")
     image = exif_rotate(image)
     append_images.append(image)
 
